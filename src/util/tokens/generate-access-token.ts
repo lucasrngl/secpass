8072fa9d62d760a6fd1efecd57fcd9930fafe6f5
Lucas Rangel <lucasrngl@hotmail.com>
2022-08-08 12:31:19 -0300

    Update tokens generation

--- src/util/tokens/generate-access-token.ts
+++ src/util/tokens/generate-access-token.ts
@@ -6,6 +6,8 @@ class GenerateAccessToken {
       { email: userEmail },
       'ea7e2c09-c254-45a4-ae0a-b37b54f00a64',
       {
+        audience: 'accessToken',
+        issuer: 'http://localhost:3000',
         subject: userId,
         expiresIn: '5m',
       }
--- src/util/tokens/generate-refresh-token.ts
+++ src/util/tokens/generate-refresh-token.ts
@@ -3,6 +3,8 @@ import { sign } from 'jsonwebtoken';
 class GenerateRefreshToken {
   static execute(userId: string) {
     const token = sign({}, 'ea7e2c09-c254-45a4-ae0a-b37b54f00a64', {
+      audience: 'refreshToken',
+      issuer: 'http://localhost:3000',
       subject: userId,
       expiresIn: '1d',
     });
