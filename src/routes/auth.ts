b84dd03bf3b08ddf0267881bffb5badf30249372
Lucas Rangel <lucasrngl@hotmail.com>
2022-08-08 12:32:24 -0300

    Add refresh token route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from '../controllers/authenticate-user-controller';
 import { CreateUserController } from '../controllers/create-user-controller';
+import { RefreshTokenController } from '../controllers/refresh-token-controller';
 
 const router = Router();
 
 router.post('/sign-up', CreateUserController.execute);
 
 router.post('/sign-in', AuthenticateUserController.execute);
+
+router.post('/refresh-token', RefreshTokenController.execute);
 
 export default router;
