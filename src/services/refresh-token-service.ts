91201a108cddcaa9ddf56027c51d4842dd7f2a1b
Lucas Rangel <lucasrngl@hotmail.com>
2022-08-08 16:05:17 -0300

    Update refresh token service

--- src/services/refresh-token-service.ts
+++ src/services/refresh-token-service.ts
@@ -6,22 +6,14 @@ class RefreshTokenService {
   static async execute(id: string, refreshToken: string) {
     const user = await UserRepository.findById(id);
 
-    const isValid: any = verify(
-      refreshToken,
-      'ea7e2c09-c254-45a4-ae0a-b37b54f00a64',
-      {
+    try {
+      verify(refreshToken, 'ea7e2c09-c254-45a4-ae0a-b37b54f00a64', {
         audience: 'refreshToken',
         issuer: 'http://localhost:3000',
         subject: user.id,
         complete: true,
-      },
-      (error, decoded) => {
-        if (error) return error;
-        return decoded;
-      }
-    );
-
-    if (isValid instanceof Error) {
+      });
+    } catch (error) {
       return new Error('Expired token');
     }
 
