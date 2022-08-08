850820be59f84b53cf49c77842cbe8aeeb3b6065
Lucas Rangel <lucasrngl@hotmail.com>
2022-08-08 15:41:45 -0300

    Update service util properties

--- src/util/services/Service.ts
+++ src/util/services/Service.ts
@@ -1,8 +1,8 @@
 import { v4 as uuid } from 'uuid';
 
 abstract class Service<T> {
-  protected props: T;
-  protected id: string;
+  props: T;
+  id: string;
 
   constructor(props: T, id?: string) {
     this.props = props;
