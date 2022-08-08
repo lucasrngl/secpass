5b30b0fab3433337ca6bc5ddaf5747a48d2df080
Lucas Rangel <lucasrngl@hotmail.com>
2022-08-08 14:28:29 -0300

    Add validation middlewares for each route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,13 +2,28 @@ import { Router } from 'express';
 import { AuthenticateUserController } from '../controllers/authenticate-user-controller';
 import { CreateUserController } from '../controllers/create-user-controller';
 import { RefreshTokenController } from '../controllers/refresh-token-controller';
+import { ValidateAuthenticationUserArguments } from '../middlewares/validate-authentication-user-arguments';
+import { ValidateCreateUserArguments } from '../middlewares/validate-create-user-arguments';
+import { ValidateRefreshTokenArguments } from '../middlewares/validate-refresh-token-arguments';
 
 const router = Router();
 
-router.post('/sign-up', CreateUserController.execute);
+router.post(
+  '/sign-up',
+  ValidateCreateUserArguments.handle,
+  CreateUserController.execute
+);
 
-router.post('/sign-in', AuthenticateUserController.execute);
+router.post(
+  '/sign-in',
+  ValidateAuthenticationUserArguments.handle,
+  AuthenticateUserController.execute
+);
 
-router.post('/refresh-token', RefreshTokenController.execute);
+router.post(
+  '/refresh-token',
+  ValidateRefreshTokenArguments.handle,
+  RefreshTokenController.execute
+);
 
 export default router;
