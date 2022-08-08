1085352981c945b696feff300c9018187b3b9c2c
Lucas Rangel <lucasrngl@hotmail.com>
2022-08-07 21:46:52 -0300

    Add sign-in route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import { AuthenticateUserController } from '../controllers/authenticate-user-controller';
 import { CreateUserController } from '../controllers/create-user-controller';
 
 const router = Router();
 
 router.post('/sign-up', CreateUserController.execute);
+
+router.post('/sign-in', AuthenticateUserController.execute);
 
 export default router;
