acb5e980bf9a3497fbe8a1f0f4e10e68831eb482
Lucas Rangel <lucasrngl@hotmail.com>
2022-08-08 15:43:32 -0300

    Set update user controller

---
+++ src/controllers/update-user-controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { UpdateUserService } from '../services/update-user-service';
+
+class UpdateUserController {
+  static async execute(request: Request, response: Response) {
+    const { id } = request.params;
+    const { ...user } = request.body;
+
+    const result = await UpdateUserService.execute(user, id);
+
+    if (result instanceof Error) {
+      return response.status(400).json(result.message);
+    }
+
+    return response.status(201).json(result);
+  }
+}
+
+export { UpdateUserController };
