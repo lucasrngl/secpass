dd10b2400f9b519336ac27aa9d17afa36d95ebc1
Lucas Rangel <lucasrngl@hotmail.com>
2022-08-08 22:44:02 -0300

    Set delete user service

---
+++ src/services/delete-user-service.ts
@@ -0,0 +1,14 @@
+import { UserRepository } from '../repositories/user-repository';
+
+class DeleteUserService {
+  static async execute(id: string) {
+    const userExists = await UserRepository.findById(id);
+    if (!userExists) {
+      return new Error('User does not exists');
+    }
+
+    await UserRepository.delete(id);
+  }
+}
+
+export { DeleteUserService };
