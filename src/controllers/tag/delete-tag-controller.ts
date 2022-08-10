5d086ab45266997919e4f0e56173a2b8d60fef1d
Lucas Rangel <lucasrngl@hotmail.com>
2022-08-09 22:55:49 -0300

    Set delete tag controller

---
+++ src/controllers/tag/delete-tag-controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { DeleteTagService } from '../../services/tag/delete-tag-service';
+
+class DeleteTagController {
+  static async execute(request: Request, response: Response) {
+    const { id: userId, tag: tagId } = request.params;
+
+    const result = await DeleteTagService.execute(userId, tagId);
+
+    if (result instanceof Error) {
+      return response.status(400).json(result.message);
+    }
+
+    return response.status(204).end();
+  }
+}
+
+export { DeleteTagController };
