3a2b148c28af2c2c57c13c2eb84b18c0b63899a6
Lucas Rangel <lucasrngl@hotmail.com>
2022-08-09 23:02:02 -0300

    Add crud methods to tag routes

--- src/routes/tag.ts
+++ src/routes/tag.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express';
 import { CreateTagController } from '../controllers/tag/create-tag-controller';
+import { DeleteTagController } from '../controllers/tag/delete-tag-controller';
+import { ListTagController } from '../controllers/tag/list-tag-controller';
+import { UpdateTagController } from '../controllers/tag/update-tag-controller';
+import { CheckUserExists } from '../middlewares/check-user-exists';
 import { EnsureAuthenticated } from '../middlewares/ensure-authenticated';
 
 const router = Router();
 
-router.post('/:id', EnsureAuthenticated.handle, CreateTagController.execute);
+router.use(CheckUserExists.handle);
+router.use(EnsureAuthenticated.handle);
+
+router.post('/:id', CreateTagController.execute);
+
+router.get('/:id', ListTagController.execute);
+
+router.put('/:id/:tag', UpdateTagController.execute);
+
+router.delete('/:id/:tag', DeleteTagController.execute);
 
 export default router;
