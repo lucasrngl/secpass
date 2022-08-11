8c46f5ef9a1b05a4e10dcd1f9b79557eea758710
Lucas Rangel <lucasrngl@hotmail.com>
2022-08-11 17:33:26 -0300

    Set API documentation with swagger

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
 import { routes } from './routes';
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/v1', routes);
+app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 export { app };
